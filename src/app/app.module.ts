bdb2e2b97cece389089d6bbc1983d644de693834
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-06 20:18:20 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GithubService } from './github.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [GithubService]
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface Repo {
+  id: number,
+  full_name: string
+}
 
 @Injectable()
 export class GithubService {
+  repos: Array<Repo> = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 }
