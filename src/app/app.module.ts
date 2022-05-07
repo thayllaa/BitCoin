d7ddba6a51fd2f8aeb6a95ae56c40411590bc650
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-06 21:26:52 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HelloComponent } from './hello.component';
 import { GithubService } from './github.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BitcoinService } from './bitcoin.service';
+import { TimerService } from './timer.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [GithubService, BitcoinService]
+  providers: [GithubService, BitcoinService, TimerService]
 })
 export class AppModule { }
