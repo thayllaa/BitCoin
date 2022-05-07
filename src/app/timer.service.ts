38361bb8b39830890c096f644ad8efac9f883bc1
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-06 22:39:27 -0300

    2 files modified

--- src/app/timer.service.ts
+++ src/app/timer.service.ts
@@ -2,22 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TimerService {
-  private timer: any;
-  private counter: 0;
-
   constructor() {}
-
-  startCount(ms: number) {
-    if (!this.timer) {
-      this.timer = setInterval(() => {
-        this.counter++;
-      }, ms);
-    }
-  }
-
-  resetCount() {
-    if (this.timer > 60) {
-      this.counter = null;
-    }
-  }
 }
