d4cca4accc9d48146b3559ac875e66aab59a07a6
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-06 21:59:46 -0300

    1 file modified

--- src/app/timer.service.ts
+++ src/app/timer.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TimerService {
+  countDown: Subscription;
+  counter: 1800;
+  tick: 1000;
 
-  constructor() { }
+  constructor() {}
 
+  ngOnInIt() {
+    this.countDown = timer(0, this.tick).subscribe(() => this.counter)
+  }
+
+  ngOnDestroy() {
+    this.countDown = null;
+  }
+
+  getCount() {
+    return this.counter;
+  }
 }
