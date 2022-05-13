ffe6086c5ddc833a262c90aac9491d90abff8f07
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-13 14:22:06 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,23 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.gitHubService.update();
+    this.myTimer;
+  }
+
+  myTimer() {
+    this.count = 60;
+    this.countDown = Observable.timer(0, 1000)
+      .subscribe(x => {
+        this.count = this.count - 1;
+      });
+
+    this.sub = Observable.interval(500)
+      .subscribe(x => {
+        console.log(this.count);
+        if (this.count === 0) {
+          this.countDown.unsubscribe();
+        }
+      });
   }
 
   updateBitcoinRates() {
