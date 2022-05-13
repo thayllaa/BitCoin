251f20a445854e9a206d072ab4cf262a391e916f
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-05-13 14:21:10 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { GithubService } from './github.service';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'HttpClient Application';
 
   constructor(
